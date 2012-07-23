80dd41a4297d95963c8791c3ab8494dcf4ef1dca
dixxi1@gmx.net <dixxi1@gmx.net@82d506f3-509b-42f4-99c4-d67f6320b0e1>
2012-07-23 21:26:45 +0000

    > Forgot to add file ;)

---
+++ src/sort/STLSort.h
@@ -0,0 +1,40 @@
+#ifndef STLSORT_H
+#define STLSORT_H
+
+#include <algorithm>
+
+#include "SortingAlgorithm.h"
+
+using namespace std;
+
+template<typename T, size_t size>
+class STLSort : public SortingAlgorithm<T, size>
+{
+    public:
+        STLSort()
+            : SortingAlgorithm<T, size>("C++ STL algorithm sort")
+        {
+        }
+
+        virtual ~STLSort()
+        {
+        }
+
+    protected:
+        bool init()
+        {
+            return true;
+        }
+
+        void sort()
+        {
+            std::sort(SortingAlgorithm<T, size>::data, SortingAlgorithm<T, size>::data + size);
+        }
+
+        void cleanup()
+        {
+
+        }
+};
+
+#endif // STLSORT_H
