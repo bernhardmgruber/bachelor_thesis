8a180a6d26036b4dd0caf48710241548b88180da
dixxi1@gmx.net <dixxi1@gmx.net@82d506f3-509b-42f4-99c4-d67f6320b0e1>
2012-10-02 23:28:39 +0000

    > Specialized run() method for float and double as compare method needs to be different

--- src/sort/cpu/QSort.h
+++ src/sort/cpu/QSort.h
@@ -24,16 +24,43 @@ namespace cpu
                 return true;
             }
 
+
             void run(T* data, T* result, size_t size) override
             {
                 qsort(data, size, sizeof(T), [](const void* a, const void* b)
                 {
-                    return *(T*)a - *(T*)b;
+                    return (int)(*(T*)a - *(T*)b);
                 });
             }
 
             virtual ~QSort() {}
     };
+
+    template<>
+    void QSort<double>::run(double* data, double* result, size_t size)
+    {
+        qsort(data, size, sizeof(double), [](const void* a, const void* b)
+        {
+            if (*((double*)a) < *((double*)b))
+                return -1;
+            else if (*((double*)a) > *((double*)b))
+                return 1;
+            return 0;
+        });
+    }
+
+    template<>
+    void QSort<float>::run(float* data, float* result, size_t size)
+    {
+        qsort(data, size, sizeof(float), [](const void* a, const void* b)
+        {
+            if (*((float*)a) < *((float*)b))
+                return -1;
+            else if (*((float*)a) > *((float*)b))
+                return 1;
+            return 0;
+        });
+    }
 }
 
 #endif // QSORT_H
--- src/sort/main.cpp
+++ src/sort/main.cpp
@@ -33,39 +33,39 @@ int main()
 {
     try
     {
-        Runner<int, SortPlugin> runner;
+        Runner<float, SortPlugin> runner;
 
         //runner.printCLInfo();
 
-        //size_t range[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 5000000, 10000000, 25000000, 50000000, 75000000, 100000000 };
-        size_t range[] = { 1024 * 256 };
+        size_t range[] = {1<<10, 1<<15, 1<<17, 1<<19, 1<<20, 1<<21, 1<<22, 1<<23, 1<<24 };
+        //size_t range[] = { 1024 * 256 };
         size_t length = sizeof(range) / sizeof(size_t);
 
-        //runner.printRange<cpu::Quicksort>(RunType::CPU, range, length);
-        //runner.printRange<cpu::QSort>(RunType::CPU, range, length);
-        //runner.printRange<cpu::STLSort>(RunType::CPU, range, length);
+        runner.printRange<cpu::Quicksort>(RunType::CPU, range, length);
+        runner.printRange<cpu::QSort>(RunType::CPU, range, length);
+        runner.printRange<cpu::STLSort>(RunType::CPU, range, length);
         //runner.printRange<cpu::TimSort>(RunType::CPU, range, length);
         //runner.printRange<cpu::amd::RadixSort>(RunType::CPU, range, length);
 
-        /*runner.printRange<gpu::bealto::ParallelSelectionSort>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelSelectionSortLocal>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelSelectionSortBlocks>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortLocal>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortLocalOptim>(RunType::CL_GPU, range, length, true);*/
-        runner.printRange<gpu::bealto::ParallelBitonicSortA>(RunType::CL_GPU, range, length, false);
-        /*runner.printRange<gpu::bealto::ParallelBitonicSortB2>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortB4>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortB8>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortB16>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelBitonicSortC>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::bealto::ParallelMergeSort>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelSelectionSort>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelSelectionSortLocal>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelSelectionSortBlocks>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortLocal>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortLocalOptim>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortA>(RunType::CL_GPU, range, length, false);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortB2>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortB4>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortB8>(RunType::CL_GPU, range, length, true);
+        //runner.printRange<gpu::bealto::ParallelBitonicSortB16>(RunType::CL_GPU, range, length, false);
+        runner.printRange<gpu::bealto::ParallelBitonicSortC>(RunType::CL_GPU, range, length, false);
+        //runner.printRange<gpu::bealto::ParallelMergeSort>(RunType::CL_GPU, range, length, true);
 
         //runner.printRange<gpu::clpp::RadixSort>(RunType::CL_GPU, range, length, true); // not working
 
         //runner.printRange<gpu::libcl::RadixSort>(RunType::CL_GPU, range, length, true); // not working
 
-        runner.printRange<gpu::amd::BitonicSort>(RunType::CL_GPU, range, length, true);
-        runner.printRange<gpu::amd::RadixSort>(RunType::CL_GPU, range, length, true);*/
+        //runner.printRange<gpu::amd::BitonicSort>(RunType::CL_GPU, range, length, false);
+        //runner.printRange<gpu::amd::RadixSort>(RunType::CL_GPU, range, length, false); // crashes on large arrays
 
         runner.writeStats("stats.csv");
         runner.writeGPUDeviceInfo("gpuinfo.csv");
