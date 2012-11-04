5e149290503f2c2c70bf68a6336ee7e81ebb260c
dixxi1@gmx.net <dixxi1@gmx.net@82d506f3-509b-42f4-99c4-d67f6320b0e1>
2012-11-04 13:48:27 +0000

    > Finalized build targets configuration for 64 bit on matrix project

--- src/matrix/main.cpp
+++ src/matrix/main.cpp
@@ -24,21 +24,18 @@ int main()
     {
         Runner<float, MatrixPlugin> runner;
 
-        //runner.printCLInfo();
-
         size_t range[] = { 1, 25, 50, 75, 100, 150, 200, 250, 300, 400, 500, 600, 700, 800, 900, 1000};//, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 };
-        //size_t range[] = { 50 };
         size_t length = sizeof(range) / sizeof(size_t);
 
-        //runner.printRange<cpu::dixxi::Mult>(RunType::CPU, range, length);
-        //runner.printRange<cpu::dixxi::MultThreads>(RunType::CPU, range, length);
+        runner.printRange<cpu::dixxi::Mult>(RunType::CPU, range, length);
+        runner.printRange<cpu::dixxi::MultThreads>(RunType::CPU, range, length);
         runner.printRange<cpu::cblas::Mult>(RunType::CPU, range, length);
 
         runner.printRange<gpu::dixxi::Mult>(RunType::CL_GPU, range, length, false);
-        //runner.printRange<gpu::dixxi::MultImage>(RunType::CL_GPU, range, length, false);
-        //runner.printRange<gpu::dixxi::MultHybrid>(RunType::CL_GPU, range, length, false);
+        runner.printRange<gpu::dixxi::MultImage>(RunType::CL_GPU, range, length, false);
+        runner.printRange<gpu::dixxi::MultHybrid>(RunType::CL_GPU, range, length, false);
 
-        //runner.printRange<gpu::amdblas::Mult>(RunType::CL_GPU, range, length, false);
+        runner.printRange<gpu::amdblas::Mult>(RunType::CL_GPU, range, length, false);
 
         runner.printRange<gpu::amd::MultTile>(RunType::CL_GPU, range, length, false);
         runner.printRange<gpu::amd::MultTileLocal>(RunType::CL_GPU, range, length, false);
