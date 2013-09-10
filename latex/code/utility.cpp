d3721c2abae43904f3057d51b9efe25f0ea30a63
dixxi1@gmx.net <dixxi1@gmx.net@82d506f3-509b-42f4-99c4-d67f6320b0e1>
2013-09-10 20:44:43 +0000

    Doblers feedback is in

--- latex/code/utility.cpp
+++ latex/code/utility.cpp
@@ -5,15 +5,15 @@ size_t roundToPowerOfTwo(size_t x) {
     x |= x >> 4;
     x |= x >> 8;
     x |= x >> 16;
-    if(sizeof(size_t) >= 8)
+    if (sizeof(size_t) >= 8)
         x |= x >> 32;
 
     return x + 1;
-}
+} // roundToPowerOfTwo
 
 size_t roundToMultiple(size_t x, size_t multiple) {
-    if(x % multiple == 0)
+    if (x % multiple == 0)
         return x;
     else
         return (x / multiple + 1) * multiple;
-}
+} // roundToMultiple
